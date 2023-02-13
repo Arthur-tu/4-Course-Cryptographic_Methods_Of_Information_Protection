7753326b8efcce6cb3131ced7c9e7ac2064261b4
Artur <Tilinov>
2023-02-13 21:22:21 +0300

    GUI

---
+++ src/sample/Main.java
@@ -0,0 +1,23 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static Stage stage;
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/controller.fxml"));
+        //primaryStage.setTitle("Подписанный документ");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 600, 360));
+        stage = primaryStage;
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) { launch(args);}
+}
