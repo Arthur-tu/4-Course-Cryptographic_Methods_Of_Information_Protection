7b2809cd815cd553e17ec81599c583fc9d5a73b8
Artur <Tilinov>
2023-03-27 07:11:02 +0300

    Основной функционал

--- src/sample/User.java
+++ src/sample/User.java
@@ -6,16 +6,36 @@ public class User {
     private String userName;
     private Key publicKey;
     private Key privateKey;
-    public static final int lenSign = 512;
+    private Key importedPublicKey;
+    private Key importedPrivateKey;
+    public static final int lenSign = 1024;
     public static final String algo = "RSA";
+
+    public Key getImportedPublicKey() {
+        return importedPublicKey;
+    }
+
+    public void setImportedPublicKey(Key importedPublicKey) {
+        this.importedPublicKey = importedPublicKey;
+    }
+
+    public Key getImportedPrivateKey() {
+        return importedPrivateKey;
+    }
+
+    public void setImportedPrivateKey(Key importedPrivateKey) {
+        this.importedPrivateKey = importedPrivateKey;
+    }
 
     public User(String userName) {
         this.userName = userName;
     }
 
-    public void setPublicKey(Key publicKey) {
-        this.publicKey = publicKey;
-    }
+    public void setPublicKey(Key publicKey) {this.publicKey = publicKey;}
+
+    public Key getPublicKey() {return publicKey;}
+
+    public Key getPrivateKey() {return privateKey;}
 
     public void setPrivateKey(Key privateKey) {
         this.privateKey = privateKey;
